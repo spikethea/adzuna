2a84949e53396b17b1433169e602237366e91642
spikethea <57224107+spikethea@users.noreply.github.com>
2021-08-22 12:39:09 +0100

    react-spring, comments and word count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState} from 'react';
 
 //Packages
-import * as Yup from 'yup';
-import { Formik, yupToFormErrors } from 'formik';
+import {useSpring, animated as a} from 'react-spring';
 
 import './App.css';
 
 function App() {
+
+  
 
   //setting Form Variables using useState 
   const [fullName, setFullName] = useState('');
@@ -56,7 +57,7 @@ function App() {
         } else {
           setEmailAddressValid(false);
           //using dot notation, I can update the value of the form error using the previous state
-          setFormErrors(f=> ({ ...f, emailAddress: "invalid email address."}));
+          setFormErrors(f=> ({ ...f, emailAddress: "Invalid email address."}));
           
         }
       } else {
@@ -66,16 +67,16 @@ function App() {
 
       //validation to check for the character length & non-empty value of notes
       if (notes !== '') {
-        if (notes.length >= 20) {
+        if (wordCount(notes) >= 6) {
           setNotesValid(true);
           setFormErrors(f=> ({ ...f, notes: undefined}));
         } else {
-          setFormErrors(f=> ({ ...f, notes: "Notes field have a minimum of 20 characters."}));
+          setFormErrors(f=> ({ ...f, notes: "Notes field must have a minimum of 6 words."}));
           setNotesValid(false);
         }
       } else {
         setNotesValid(true);
-        setFormErrors(f=> ({ ...f, notes: " notes field must be non-empty"}));
+        setFormErrors(f=> ({ ...f, notes: " Notes field must be non-empty"}));
       }
 
   }, [fullName, emailAddress, notes])
@@ -90,16 +91,17 @@ function App() {
     switch(fieldType) {
 
       case 'fullName':
-        // set the value of the Email field to update the value on change
+        // set the value of the Full Name field to update the value on change, using the event value
         setFullName(e.target.value);
       break;
 
       case 'emailAddress':
-        // set the value of the email field to update the value on change
+        // set the value of the Email Address field to update the value on change, using the event value
         setEmailAddress(e.target.value);
       break;
 
       case 'notes':
+         // set the value of the notes field to update the value on change, using the event value
         setNotes(e.target.value);
       break;
 
@@ -112,7 +114,7 @@ function App() {
     // prevent the default action of HTML buttons
     e.preventDefault()
 
-    //checking all the fields are valid
+    //checking all the fields are valid.
     if (fullNameValid && emailAddressValid && notesValid) {
       setFormState(states.SUBMITTED);
       //window.location.reload();
@@ -121,12 +123,11 @@ function App() {
     }
   }
 
-  //function to handle resetting the form
+  //function to handle resetting the forms fields
   const resetForm = () => {
 
     // changing the state of the form back to its intial value
     setFormState(states.UNFINISHED);
-
 
     //resetting the fields back to empty strings
     setFullName('');
@@ -134,23 +135,41 @@ function App() {
     setEmailAddress('');
   }
 
+  //function which returns the converted character length to word count.
+  const wordCount = (text = '') => {
+    return text.split(/\S+/).length - 1;
+  };
+
+  //defined settings for the transition animation of the Form with react-spring
+  const styleProps1 = useSpring({
+    to: { opacity: formState === states.SUBMITTED ? 1 : 0 },
+    from: { opacity: 0 }
+ });
+ 
+ //defined settings for transition animation of the validation confirmation with react-spring
+ const styleProps2 = useSpring({
+    to: { opacity: formState === states.SUBMITTED ? 0 : 1 },
+    from: { opacity: 0 }
+ });
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <section className="form-container">
-      {formState === states.SUBMITTED ? // (using ternary operators) conditionally render the component depending on the validation success
-      <article>
+      {formState === states.SUBMITTED ? // (using ternary operators) conditionally render the component depending on the validation state
+      <a.article style={styleProps1}>
         <h1>Thank You!</h1>
         <p>Your account details have been submitted successfully</p>
         <button onClick={resetForm}>Resubmit</button>
-      </article>
-      : // render the 
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <h1>Adzuna Form</h1>
+      </a.article>
+      : // render the form on condition that the form is not submitted
+      <a.form style={styleProps2} onSubmit={(e) => handleSubmit(e)}>
+        <h1>Adzuna Simple Form</h1>
+        <section>
           <fieldset>
             <label htmlFor="fname">Full Name:</label>
             <input type="text" id="fname" name="fname" onChange={(e)=> validateField('fullName', e)} value={fullName}/>
-            {formState === states.ERROR ? <p className="error-message">{formErrors.fullName}</p>: null}
+            {formState === states.ERROR ? <p className="error-message">{formErrors.fullName}</p>: null}{/*  using ternary operators, th  */}
           </fieldset>
           <fieldset>
             <label htmlFor="fname">Email Address:</label>
@@ -161,10 +180,13 @@ function App() {
             <label htmlFor="fname">Notes:</label>
             <textarea type="notes" id="notes" onChange={(e) => validateField('notes', e)} value={notes}/>
             {formState === states.ERROR ? <p className="error-message">{formErrors.notes}</p>: null}
-            <p >{notes.length} character(s)</p>
+            <p className="info-text">{wordCount(notes)} word(s)</p>
           </fieldset>
-          <button type="submit" >Submit</button>
-        </form>
+          </section>
+          <footer>
+            <button type="submit">Submit</button>
+          </footer>
+        </a.form>
       }
       </section>
       <footer>
